907f5ac70ab6c5b4aa9a493a681fecee64df3a15
Joseph John <joseph.john@anu.edu.au>
2026-06-19 12:33:58 +1000

    Job scripts for DPP and HF Accelerate updated
    Job scripts for followting tested and working.
    
    - Multi-GPU PyTorch on single node using DDP
    - Multi-GPU PyTorch on single node using Hugging Face Accelerate
    - Multi-GPU PyTorch on multiple node using DDP

---
+++ job_script/ddp_multi_node_launcher.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Load shell environment variables
+source ~/.bashrc
+
+module load cuda/11.7.0
+module load intel-mkl/2022.2.0
+module load openmpi
+
+source /scratch/vp91/pytorch-venv/bin/activate
+ 
+export USER=$(whoami)
+
+ # Run PyTorch application
+torchrun \
+  --nnodes=${1} \
+  --nproc_per_node=${2} \
+  --rdzv_id=100 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=${3}:29400 \
+  /scratch/vp91/$USER/applied-ml/src/ddp/train.py
